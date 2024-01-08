4edd182a16d34607076f24339b408f2d7fb60c2f
Felipe Ricieri <felipericieri@icloud.com>
2024-01-08 21:01:51 +0000

    Foundation

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Rici on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
