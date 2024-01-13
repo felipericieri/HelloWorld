e2a9bb023ee0159d21f74a8da9ca719ca7d55e5f
Felipe Ricieri <felipericieri@icloud.com>
2024-01-13 22:35:20 +0000

    Updates CI

---
+++ fastlane/modules/utils.rb
@@ -0,0 +1,27 @@
+# Utils
+
+module Utils
+  # Define whether fastlane is running locally or in CI
+  def self.is_ci
+    ENV['CI']
+  end
+
+  # Sets up keychain in CI
+  def self.ci_keychain_setup
+    if is_ci
+      create_ci_keychain
+    end
+  end
+
+  # Creates Keychain
+  def self.create_ci_keychain
+    create_keychain(
+      name: "CI_Keychain",
+      password: ENV["KEYCHAIN_PASSWORD"], # Set this in your CI environment variables
+      default_keychain: true,
+      unlock: true,
+      timeout: 3600,
+      lock_when_sleeps: true
+    )
+  end
+end
