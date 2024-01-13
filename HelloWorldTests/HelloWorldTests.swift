030ca9952001668c853b9d3b09a0219f8e865fe9
Felipe Ricieri <felipericieri@icloud.com>
2024-01-13 11:00:19 +0000

    Changes

--- HelloWorldTests/HelloWorldTests.swift
+++ HelloWorldTests/HelloWorldTests.swift
@@ -9,25 +9,8 @@ import XCTest
 @testable import HelloWorld
 
 final class HelloWorldTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test() {
+        XCTAssert(true)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        XCTAssertTrue(true)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        XCTAssertTrue(true)
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
